18223099050e7d3f20e6faa327ca84597223454f
Nicolas D <oc_nicolasd>
2023-02-05 22:30:38 +0100

    feat: #5 Build desktop error page

--- src/pages/ErrorPage/ErrorPage.js
+++ src/pages/ErrorPage/ErrorPage.js
@@ -1,7 +1,14 @@
-import { useRouteError } from 'react-router-dom'
+import { useRouteError, NavLink } from 'react-router-dom'
+import classes from './ErrorPage.module.css'
 
 export default function ErrorPage() {
   const error = useRouteError()
 
-  return <>Error Page</>
+  return (
+    <div className={ classes.content }>
+      <h1 className={ classes.statusCode }>404</h1>
+      <p className={ classes.statusMessage }>Oups! La page que vous demandez n'existe pas.</p>
+      <NavLink to="/" className={ classes.link }>Retourner sur la page d'accueil</NavLink>
+    </div>
+  )
 }
