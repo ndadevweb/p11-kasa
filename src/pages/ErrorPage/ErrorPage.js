766d1b199c6fe5193adcfea1e53f7b792519341a
Nicolas D <oc_nicolasd>
2023-02-18 16:15:07 +0100

    feat: Make the error page responsive #12

--- src/pages/ErrorPage/ErrorPage.js
+++ src/pages/ErrorPage/ErrorPage.js
@@ -1,7 +1,12 @@
+import { useEffect } from 'react'
 import { NavLink } from 'react-router-dom'
 import classes from './ErrorPage.module.css'
 
 export default function ErrorPage() {
+  useEffect(() => {
+    document.title = 'Erreur 404'
+  }, [])
+
   return (
     <div className={ classes.content }>
       <h1 className={ classes.statusCode }>404</h1>
